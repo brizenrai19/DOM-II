608af7522b31ecdf6e2170158bd76fe8fe3373c6
unknown <brizen.rai19@gmail.com>
2021-04-05 22:44:17 -0400

    Update

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+let signMeUpBtn = document.getElementsByClassName("btn");
+
+for(let i=0; i<signMeUpBtn.length; i++){
+    signMeUpBtn[i].addEventListener("click",function(){
+        alert("Sign Me Up Button Clicked");
+    })
+}
